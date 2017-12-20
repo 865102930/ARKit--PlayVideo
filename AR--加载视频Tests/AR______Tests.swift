11ae2806530e38453edebee90a38f0ba1bfebf88
gz <gaoze0506@163.com>
2017-12-20 22:35:47 +0800

    Initial Commit

---
+++ AR--加载视频Tests/AR______Tests.swift
@@ -0,0 +1,36 @@
+//
+//  AR______Tests.swift
+//  AR--加载视频Tests
+//
+//  Created by gz on 2017/12/20.
+//  Copyright © 2017年 gz. All rights reserved.
+//
+
+import XCTest
+@testable import AR______
+
+class AR______Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
